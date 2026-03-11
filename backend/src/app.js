d158e6b52c65d7b653459745128198e51c96758c
Ashish04500 <ashishalways45@gmail.com>
2026-03-11 12:11:40 +0530

    added routes and connect to models

--- backend/src/app.js
+++ backend/src/app.js
@@ -3,18 +3,18 @@ import {createServer} from 'node:http';
 import {Server} from 'socket.io';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import {connectTosocket} from './controllers/socketmanger.js';
 
 const app=express();
 const server=createServer(app);
-const io=new Server(server);
+const io=connectTosocket(server);
 app.set("port",(process.env.PORT || 8000));
-app.get('/home',(req,res)=>{
-    return res.json({"hello":"world"})
-});
-
+app.use(cors());
+app.use(express.json({limit:"40kb"}));
+app.use(express.urlencoded({limit:"40kb",extended:true}));
 const start=async()=>{
-    const connectdb=await mongoose.connect("mongodb+srv://ashish1010:<ashish@2645>@cluster0.epiqwdy.mongodb.net/")
-    console.log("MONGO connected db host:${connectdb.connection.host}");
+    const connectdb=await mongoose.connect("mongodb+srv://ashish1010:ashish2645@cluster0.epiqwdy.mongodb.net/")
+    console.log(`MONGO connected db host:${connectdb.connection.host}`);
     server.listen(app.get("port"),()=>{
         console.log("Listening on port 8000");
     });
