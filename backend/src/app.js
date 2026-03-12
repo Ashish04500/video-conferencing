a389096a544fa343131bd98ae946a604e4111620
Ashish04500 <ashishalways45@gmail.com>
2026-03-13 02:25:28 +0530

    Login and registration routes added to the backend

--- backend/src/app.js
+++ backend/src/app.js
@@ -4,6 +4,7 @@ import {Server} from 'socket.io';
 import mongoose from 'mongoose';
 import cors from 'cors';
 import {connectTosocket} from './controllers/socketmanger.js';
+import userRoutes from './route/users.route.js';
 
 const app=express();
 const server=createServer(app);
@@ -12,6 +13,7 @@ app.set("port",(process.env.PORT || 8000));
 app.use(cors());
 app.use(express.json({limit:"40kb"}));
 app.use(express.urlencoded({limit:"40kb",extended:true}));
+app.use("/api/v1/users",userRoutes);
 const start=async()=>{
     const connectdb=await mongoose.connect("mongodb+srv://ashish1010:ashish2645@cluster0.epiqwdy.mongodb.net/")
     console.log(`MONGO connected db host:${connectdb.connection.host}`);
