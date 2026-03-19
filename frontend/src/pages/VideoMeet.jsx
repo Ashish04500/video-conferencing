152e77a23f8f029a5abb18fb81129082b1f569c9
Ashish04500 <ashishalways45@gmail.com>
2026-03-19 20:38:43 +0530

    improving meet ui

--- frontend/src/pages/VideoMeet.jsx
+++ frontend/src/pages/VideoMeet.jsx
@@ -13,7 +13,7 @@ import StopScreenShareIcon from '@mui/icons-material/StopScreenShare'
 import ChatIcon from '@mui/icons-material/Chat'
 
 
-const server_url = "http://localhost:8000/api/v1/users";
+const server_url = "http://localhost:8000";
 
 var connections = {};
 
